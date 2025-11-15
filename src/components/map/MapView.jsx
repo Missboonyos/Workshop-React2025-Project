50bcd1fcdf1e8f06f804ecd93554f9b09d11f024
Misboonyos <boonyos.k@gmail.com>
2025-11-15 21:01:37 +0700

    EP16: Markers, Popup, Tooltips

--- src/components/map/MapView.jsx
+++ src/components/map/MapView.jsx
@@ -2,14 +2,17 @@
 import React from "react";
 import { 
   LayersControl, 
-  MapContainer, 
-  Marker, 
+  MapContainer,   
+  Marker,   
+  Popup,   
   TileLayer, 
+  Tooltip, 
   useMap, 
   useMapEvents
 } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import Layers from "./Layers"
+import useDutyStore from "../../store/useDutyStore";
 
 // Events function
 const ClickToAdd = ({ adding, onPick })=> {
@@ -39,20 +42,38 @@ const ClickToAdd = ({ adding, onPick })=> {
 
 const MapView = ({ adding, onPick }) => {
 
+  // All data in s.locations are assigned to be in const locations
+  const locations = useDutyStore((s)=>s.locations)
+
   const center = [13,100]
   
   return (
     <div className="flex-1">
-      <MapContainer
-        className="h-full"
-        center={center}
-        zoom={7}
-        scrollWheelZoom={true}
-      >
-        
-      <Layers />
+      <MapContainer className="h-full" center={center} zoom={7}
+        scrollWheelZoom={true}>
+        <Layers />
+        <ClickToAdd adding={adding} onPick={onPick} />
 
-      <ClickToAdd adding={adding} onPick={onPick} />
+      {
+        locations.map((item)=>{
+          return <Marker 
+          key={item.id}
+          position={[item.lat, item.lng]}>
+
+            <Popup>
+              <div className="text-sm">{item.name}</div>
+              <div className="text-sm text-gray-500">
+                {item.lat.toFixed(6)},{item.lng.toFixed(6)}</div>
+            </Popup>
+
+            <Tooltip direction="auto">
+              <div>{item.name}</div>
+              <div>{item.lat.toFixed(6)},{item.lng.toFixed(6)}</div>
+            </Tooltip>
+            
+          </Marker>
+        })
+      }
 
       </MapContainer>
     </div>
